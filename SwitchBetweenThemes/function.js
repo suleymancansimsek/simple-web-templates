052593801aa632cc523e2c0f626b980486542d1f
suleymancansimsek <suleymancansimsek@gmail.com>
2023-07-13 13:56:11 +0300

    swtich between themes improved

--- SwitchBetweenThemes/function.js
+++ SwitchBetweenThemes/function.js
@@ -2,15 +2,33 @@ const themeButton = document.getElementById('theme-button');
 let currentTheme = 0;
 
 const themes = [
-  'theme-1',
-  'theme-2',
-  'theme-3',
-  'theme-4'
+  {
+    name: "Theme 1",
+    bg: "#6528F7",
+    btn: "#D7BBF5"
+  },
+  {
+    name: "Theme 2",
+    bg: "#071952",
+    btn: "#35A29F"
+  },
+  {
+    name: "Theme 3",
+    bg: "#FFEADD",
+    btn: "#FF6666"
+  }
 ];
 
 function switchTheme() {
-  document.body.className = themes[currentTheme];
+  document.body.style.backgroundColor = themes[currentTheme].bg;
+  themeButton.style.backgroundColor = themes[currentTheme].btn;
+  themeButton.textContent = themes[currentTheme].name;
   currentTheme = (currentTheme + 1) % themes.length;
 }
+
+// Set initial theme
+document.body.style.backgroundColor = themes[currentTheme].bg;
+themeButton.style.backgroundColor = themes[currentTheme].btn;
+themeButton.textContent = themes[currentTheme].name;
 
 themeButton.addEventListener('click', switchTheme);
